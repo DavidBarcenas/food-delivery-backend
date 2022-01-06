3d620845922b6bfb69756b5bd3b4645410b0ac4e
David Barcenas <daveepro@outlook.com>
2022-01-06 14:48:08 -0600

    create table in db from typeorm

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,16 +1,30 @@
 import { Field, ObjectType } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @ObjectType()
+@Entity()
 export class Restaurant {
+  @Field(type => Number)
+  @PrimaryGeneratedColumn()
+  id: number;
+
   @Field(type => String)
+  @Column()
   name: string;
 
   @Field(type => Boolean)
+  @Column()
   isVegan: string;
 
   @Field(type => String)
+  @Column()
   address: string;
 
   @Field(type => String)
+  @Column()
   ownersName: string;
+
+  @Field(type => String)
+  @Column()
+  categoryName: string;
 }
