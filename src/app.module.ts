790e4b26f3e43181b12addd745effa4bd872c2ee
David Barcenas <daveepro@outlook.com>
2022-01-06 14:28:06 -0600

    validate schema to connect to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
+import * as Joi from 'joi';
 
 import { DatabaseModule } from './database/database.module';
 import { RestaurantsModule } from './restaurants/restaurants.module';
@@ -12,9 +13,20 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
       envFilePath:
         process.env.NODE_ENV === 'development' ? '.env.dev' : '.env.test',
       ignoreEnvFile: process.env.NODE_ENV === 'production',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test')
+          .default('development')
+          .required(),
+        DATABASE_HOST: Joi.string().required(),
+        DATABASE_PORT: Joi.number().required(),
+        DATABASE_USER: Joi.string().required(),
+        DATABASE_PASSWORD: Joi.string().required(),
+        DATABASE_NAME: Joi.string().required(),
+      }),
     }),
-    GraphQLModule.forRoot(),
     DatabaseModule,
+    GraphQLModule.forRoot(),
     RestaurantsModule,
   ],
   controllers: [],
