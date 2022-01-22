a304868227f2ab666fc026567e4208128ad578db
David Barcenas <daveepro@outlook.com>
2022-01-21 20:32:32 -0600

    create global mail module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,16 +6,13 @@ import {
 } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
-import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
-import { join } from 'path';
 
+import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
 import { JwtModule } from './jwt/jwt.module';
 import { JwtMiddleware } from './jwt/jwt.middleware';
-import { MailerModule } from '@nestjs-modules/mailer';
-import { DatabaseModule } from './database/database.module';
-import { schema } from './config/schema-validation';
 import { environments } from './config/environments';
+import { schema } from './config/schema-validation';
 
 @Module({
   imports: [
@@ -31,30 +28,7 @@ import { environments } from './config/environments';
       context: ({ req }) => ({ user: req['user'] }),
     }),
     JwtModule.forRoot({ secretKey: process.env.SECRET_KEY }),
-    MailerModule.forRoot({
-      transport: {
-        host: process.env.MAIL_HOST,
-        port: parseInt(process.env.MAIL_PORT, 10),
-        ignoreTLS: false,
-        secure: true,
-        auth: {
-          user: process.env.MAIL_USER,
-          pass: process.env.MAIL_PASSWORD,
-        },
-      },
-      defaults: {
-        from: '"no-reply" <delivery@company.com>',
-      },
-      template: {
-        dir: join(__dirname, '/mail/templates'),
-        adapter: new HandlebarsAdapter(),
-        options: {
-          strict: true,
-        },
-      },
-    }),
     UsersModule,
-    DatabaseModule,
   ],
   controllers: [],
   providers: [],
---
+++ src/mail/mail.module.ts
@@ -0,0 +1,40 @@
+import { MailerModule } from '@nestjs-modules/mailer';
+import { Global, Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import { join } from 'path';
+import { MailService } from './mail.service';
+
+@Global()
+@Module({
+  imports: [
+    MailerModule.forRootAsync({
+      useFactory: async (config: ConfigService) => ({
+        transport: {
+          host: config.get('MAIL_HOST'),
+          port: parseInt(config.get('MAIL_PORT'), 10),
+          ignoreTLS: false,
+          secure: true,
+          auth: {
+            user: config.get('MAIL_USER'),
+            pass: config.get('MAIL_PASSWORD'),
+          },
+        },
+        defaults: {
+          from: '"no-reply" <noreply@delivery.com>',
+        },
+        template: {
+          dir: join(__dirname, 'templates'),
+          adapter: new HandlebarsAdapter(),
+          options: {
+            strict: true,
+          },
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  providers: [MailService],
+  exports: [MailService],
+})
+export class MailModule {}
