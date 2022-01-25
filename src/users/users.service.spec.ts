8711270ad0b5b23ea9eea421289a3f8ed77382b1
David Barcenas <daveepro@outlook.com>
2022-01-24 21:47:47 -0600

    create users mock repository

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -5,6 +5,7 @@ import { getRepositoryToken } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { EmailVerification } from './entities/email-verification.entity';
 import { MailService } from 'src/mail/mail.service';
+import { Repository } from 'typeorm';
 
 const mockRepository = {
   findOne: jest.fn(),
@@ -21,27 +22,42 @@ const mockMailService = {
   emailVerification: jest.fn(),
 };
 
+type MockRepository<T> = Partial<Record<keyof Repository<T>, jest.Mock>>;
+
 describe('UserService', () => {
   let service: UsersService;
+  let usersRepository: MockRepository<User>;
 
   beforeAll(async () => {
     const module = await Test.createTestingModule({
       providers: [
         UsersService,
-        { provide: getRepositoryToken(User), useValue: mockRepository },
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockRepository,
+        },
         {
           provide: getRepositoryToken(EmailVerification),
           useValue: mockRepository,
         },
-        { provide: JwtService, useValue: mockJwtService },
-        { provide: MailService, useValue: mockMailService },
+        {
+          provide: JwtService,
+          useValue: mockJwtService,
+        },
+        {
+          provide: MailService,
+          useValue: mockMailService,
+        },
       ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+    usersRepository = module.get(getRepositoryToken(User));
   });
 
   it('shuld be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('should fail if user exists', () => null);
 });
