72d051455b1d431eddcd0e0cc42a3f4c0323507e
David Barcenas <daveepro@outlook.com>
2022-02-10 21:00:35 -0600

    test token signature

--- src/mail/mail.service.spec.ts
+++ src/mail/mail.service.spec.ts
@@ -1,8 +1,15 @@
 import { Test } from '@nestjs/testing';
+import * as jwt from 'jsonwebtoken';
 import { CONFIG_OPTIONS } from 'src/common/common.constants';
 import { JwtService } from 'src/jwt/jwt.service';
 
 const TEST_KEY = 'testKey';
+
+jest.mock('jsonwebtoken', () => {
+  return {
+    sign: jest.fn(() => 'TOKEN'),
+  };
+});
 
 describe('JwtService', () => {
   let service: JwtService;
@@ -13,7 +20,7 @@ describe('JwtService', () => {
         JwtService,
         {
           provide: CONFIG_OPTIONS,
-          useValue: { privateKey: TEST_KEY },
+          useValue: { secretKey: TEST_KEY },
         },
       ],
     }).compile();
@@ -22,5 +29,16 @@ describe('JwtService', () => {
 
   it('be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('sign', () => {
+    it('should return a signed token', () => {
+      service.create({ email: 'bs.foo@mail.com' });
+      expect(jwt.sign).toHaveBeenCalledTimes(1);
+      expect(jwt.sign).toHaveBeenCalledWith(
+        { email: 'bs.foo@mail.com' },
+        TEST_KEY,
+      );
+    });
   });
 });
