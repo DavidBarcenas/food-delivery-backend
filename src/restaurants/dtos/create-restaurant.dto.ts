0ac932d257a3275140da3bae3c664d11d822edac
David Barcenas <daveepro@outlook.com>
2022-02-14 17:26:24 -0600

    update precommit script

--- src/restaurants/dtos/create-restaurant.dto.ts
+++
@@ -1,9 +0,0 @@
-import { InputType, OmitType } from '@nestjs/graphql';
-import { Restaurant } from '../entities/restaurant.entity';
-
-@InputType()
-export class CreateRestaurantDto extends OmitType(
-  Restaurant,
-  ['id'],
-  InputType,
-) {}
