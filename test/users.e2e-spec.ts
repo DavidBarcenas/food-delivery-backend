d182a4b937152eb297db3ee6f998e409e202d077
David Barcenas <daveepro@outlook.com>
2022-02-15 18:06:11 -0600

    test e2e - create user account

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -1,6 +1,12 @@
+import * as request from 'supertest';
+
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+
 import { AppModule } from '../src/app.module';
+import { INestApplication } from '@nestjs/common';
+import { getConnection } from 'typeorm';
+
+const GRAPHQL_ENDPOINT = '/graphql';
 
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
@@ -14,7 +20,40 @@ describe('UserModule (e2e)', () => {
     await app.init();
   });
 
-  it.todo('createAccount');
+  afterAll(async () => {
+    await getConnection().dropDatabase();
+    app.close();
+  });
+
+  describe('createAccount', () => {
+    const EMAIL = 'davee@mail.com';
+    it('should create account', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+          mutation {
+            createAccount(input: { 
+              email: "${EMAIL}",
+              password: "12345",
+              role: Owner
+            }) {
+              ok
+              error
+            }
+          }
+        `,
+        })
+        .expect(200)
+        .expect(res => {
+          expect(res.body.data.createAccount.ok).toBeTruthy();
+          expect(res.body.data.createAccount.error).toBeNull();
+        });
+    });
+
+    it.todo('should fail if account already exists');
+  });
+
   it.todo('login');
   it.todo('findByEmail');
   it.todo('editProfile');
