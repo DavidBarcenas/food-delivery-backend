87e2156e4c052da3f0c39df757014f9b31d21e91
David Barcenas <daveepro@outlook.com>
2022-02-15 20:03:53 -0600

    test e2e login

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -8,8 +8,14 @@ import { getConnection } from 'typeorm';
 
 const GRAPHQL_ENDPOINT = '/graphql';
 
+const testUser = {
+  email: 'davee@mail.com',
+  password: '12345',
+};
+
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
+  let token: string;
 
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -26,7 +32,6 @@ describe('UserModule (e2e)', () => {
   });
 
   describe('createAccount', () => {
-    const EMAIL = 'davee@mail.com';
     it('should create account', () => {
       return request(app.getHttpServer())
         .post(GRAPHQL_ENDPOINT)
@@ -34,8 +39,8 @@ describe('UserModule (e2e)', () => {
           query: `
           mutation {
             createAccount(input: { 
-              email: "${EMAIL}",
-              password: "12345",
+              email: "${testUser.email}",
+              password: "${testUser.password}",
               role: Owner
             }) {
               ok
@@ -58,8 +63,8 @@ describe('UserModule (e2e)', () => {
           query: `
           mutation {
             createAccount(input: { 
-              email: "${EMAIL}",
-              password: "12345",
+              email: "${testUser.email}",
+              password: "${testUser.password}",
               role: Owner
             }) {
               ok
@@ -78,7 +83,61 @@ describe('UserModule (e2e)', () => {
     });
   });
 
-  it.todo('login');
+  describe('login', () => {
+    it('should login with correct credentials', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+        mutation {
+          login(input: { 
+            email: "${testUser.email}",
+            password: "${testUser.password}",
+          }) {
+            ok
+            error
+            token
+          }
+        }
+      `,
+        })
+        .expect(200)
+        .expect(res => {
+          expect(res.body.data.login.ok).toBeTruthy();
+          expect(res.body.data.login.error).toBeNull();
+          expect(res.body.data.login.token).toEqual(expect.any(String));
+          token = res.body.data.login.token;
+        });
+    });
+
+    it('should not be able to login with wrong credentials', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+        mutation {
+          login(input: { 
+            email: "${testUser.email}",
+            password: "321312312313",
+          }) {
+            ok
+            error
+            token
+          }
+        }
+      `,
+        })
+        .expect(200)
+        .expect(res => {
+          expect(res.body.data.login.ok).toBeFalsy();
+          expect(res.body.data.login.error).toEqual(
+            'The email or password is incorrect.',
+          );
+          expect(res.body.data.login.token).toBeNull();
+        });
+    });
+  });
+
   it.todo('findByEmail');
   it.todo('editProfile');
   it.todo('verifyEmail');
